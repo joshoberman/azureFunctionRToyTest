58b90e46ce56178f8d1ad541c462e1d7289e029d
Josh Oberman <joshoberman@gmail.com>
2017-05-04 18:55:51 -0500

    got rid of sink

--- script/testAzureFunction.R
+++ script/testAzureFunction.R
@@ -1,11 +1,8 @@
-sink("redirect.txt")
 library(devtools) 
 library(soiCalcEngine)
 
 input_json <- Sys.getenv("in_json")
 
 output_json <- runCalc(input_json)
-
-sink()
 
 write(output_json, path = stdout())
