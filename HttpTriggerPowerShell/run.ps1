58dfab5c35bb4631ab43671032a3de015af01f5c
Josh Oberman <joshoberman@gmail.com>
2017-05-04 18:59:23 -0500

    reverting to cd to script

--- HttpTriggerPowerShell/run.ps1
+++ HttpTriggerPowerShell/run.ps1
@@ -3,9 +3,9 @@ $requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $env:in_json = $requestBody.json
 #$env:in_json = Get-Content "sample_input\sample.json"
 
-cd D:\home\site\wwwroot
+cd D:\home\site\wwwroot\script
 
 #$env:Rscript = ".\resources\R-Portable\App\R-Portable\bin\Rscript.exe"
-$output = & "D:\home\R-3.3.3\bin\x64\Rscript.exe" ".\script\testAzureFunction.R"
+$output = &D:\home\R-3.3.3\bin\x64\Rscript.exe testAzureFunction.R
 
 Out-File -Encoding Ascii -FilePath $res -inputObject $output
