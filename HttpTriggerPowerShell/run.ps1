5385552b13f531e585dd7bd72206259dd7702893
Josh Oberman <joshoberman@gmail.com>
2017-05-04 17:10:52 -0500

    adding script

--- HttpTriggerPowerShell/run.ps1
+++ HttpTriggerPowerShell/run.ps1
@@ -3,9 +3,9 @@ $requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $env:in_json = $requestBody.json
 #$env:in_json = Get-Content "sample_input\sample.json"
 
-cd D:\home\site\wwwroot\dist
+cd D:\home\site\wwwroot\
 
 $env:Rscript = ".\resources\R-Portable\App\R-Portable\bin\Rscript.exe"
-$output = & $env:Rscript ".\resources\process_json.R"
+$output = & ".\resources\R-Portable\App\R-Portable\bin\Rscript.exe" ".\resources\process_json.R"
 
 Out-File -Encoding Ascii -FilePath $res -inputObject $output
