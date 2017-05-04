cadc5e92b899aec56d84e4408ef3bdc187d7132f
Josh Oberman <joshoberman@gmail.com>
2017-05-04 17:18:27 -0500

    added slash after cd in powershell script

--- HttpTriggerPowerShell/run.ps1
+++ HttpTriggerPowerShell/run.ps1
@@ -3,7 +3,7 @@ $requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $env:in_json = $requestBody.json
 #$env:in_json = Get-Content "sample_input\sample.json"
 
-cd D:\home\site\wwwroot\scripts
+cd D:\home\site\wwwroot\scripts\
 
 #$env:Rscript = ".\resources\R-Portable\App\R-Portable\bin\Rscript.exe"
 $output = & D:\home\R-3.3.3\bin\x64\Rscript.exe testAzureFunction.R
