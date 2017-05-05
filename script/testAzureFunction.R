99cdd2d3f45829ced28e8975c20a7e10e547b11b
Josh Oberman <joshoberman@gmail.com>
2017-05-04 19:41:41 -0500

    more changes

--- script/testAzureFunction.R
+++ script/testAzureFunction.R
@@ -6,6 +6,7 @@ input_json <<- Sys.getenv("in_json")
 
 tm <- system.time({output_json <- runCalc(input_json)})
 
-out_list <- jsonlite::toJSON(list(time = tm, out_json = output_json))
+out_list <- list(time = tm, out_json = output_json)
+out_list <- jsonlite::toJSON(out_list)
 
 write(out_list, stdout())
