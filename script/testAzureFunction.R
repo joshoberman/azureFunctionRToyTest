30d95f0e6c335d49a89ec2d33d97c1d72629885d
Josh Oberman <joshoberman@gmail.com>
2017-05-04 19:51:40 -0500

    getting time

--- script/testAzureFunction.R
+++ script/testAzureFunction.R
@@ -1,12 +1,8 @@
 library(devtools) 
-library(jsonlite)
 library(soiCalcEngine)
 
 input_json <<- Sys.getenv("in_json")
 
 tm <- system.time({output_json <- runCalc(input_json)})
 
-out_list <- list(time = tm, out_json = output_json)
-out_list <- jsonlite::toJSON(out_list)
-
-write(out_list, stdout())
+write(tm["elapsed"], stdout())
